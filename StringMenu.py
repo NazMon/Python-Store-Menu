250ae6fd8e74fc84a0c7c76435dfd7ac2365a05d
nazmon <mondal.nazib@gmail.com>
2024-03-02 22:07:10 -0500

    Added a 'prices' tuple to match the 'foods' tuple, so the index has the correct corresponding prices. Doing so allows us to iterate through the tuples instead of utilizing individual print statements.

--- StringMenu.py
+++ StringMenu.py
@@ -3,18 +3,16 @@
 # such as title(), r/ljust(), center(), et. al.,
 
 # Header for the Menu's title, formatted accordingly and menu list
-# Utilizing tuples, no need for mutability
+# Utilizing tuples, no need for mutability for now
 header = "  quality cookery menu  ".title().center(30, "=")
-food = ("Takoyaki 5pc", "Takoyaki 10pc", "Taiyaki Small",
-        "Taiyaki Large", "PIzza (12 inch)", "Pizza (16 inch)")
+foods = ("Takoyaki 5pc", "Takoyaki 10pc", "Taiyaki Small",
+        "Taiyaki Large", "Pizza (12 inch)", "Pizza (16 inch)")
+prices = ("$5", "$8", "$5", "$8", "$10", "$15")
 
-# Adding Menu items for display - utilizng print
-# and justifications for now
+# Adding Menu items for display - utilizng print and justifications
+# Using a loop that matches the two tuples prices to the foods
+# Consider creating an object for this in the future OR key-value pairs
 print("\n" + header)
-print(food[0].ljust(28, ".") + "$5".rjust(2))
-print(food[1].ljust(28, ".") + "$8".rjust(2))
-print(food[2].ljust(28, ".") + "$5".rjust(2))
-print(food[3].ljust(28, ".") + "$8".rjust(2))
-print(food[4].ljust(28, ".") + "$10".rjust(2))
-print(food[5].ljust(28, ".") + "$15".rjust(2))
+for x in range(len(foods)):
+    print(foods[x].ljust(28, ".") + prices[x].rjust(2))
 print()
